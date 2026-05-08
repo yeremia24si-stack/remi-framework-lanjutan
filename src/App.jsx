0543d489b716fae0599ab05a7ae64b1c8255bfbe
yeremi <yeremia24si@mahasiswa.pcr.ac.id>
2026-05-08 16:26:45 +0700

    Pertemuan 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
-import Sidebar from "./layouts/Sidebar";
-import Header from "./layouts/Header";
-import Dashboard from "./pages/Dashboard";
-import Orders from "./pages/Orders";
-import Customers from "./pages/Customer";
-import ErrorPage from "./components/ErrorPage";
+import Loading from "./components/Loading";
+import "./assets/tailwind.css";
+
+// import Dashboard from "./pages/Dashboard";
+// import Orders from "./pages/Orders";
+// import Customers from "./pages/Customer";
+// import ErrorPage from "./components/ErrorPage";
+// import MainLayout from "./layouts/MainLayout";
+// import AuthLayout from "./layouts/AuthLayout";
+// import Login from "./pages/auth/Login";
+// import Register from "./pages/auth/Register";
+// import Forgot from "./pages/auth/Forgot";
+
+const Dashboard = React.lazy(() => import("./pages/Dashboard"))
+const Orders = React.lazy(() => import("./pages/Orders"))
+const Customers = React.lazy(() => import("./pages/Customer"))
+const ErrorPage = React.lazy(() => import("./components/ErrorPage"))
+const MainLayout = React.lazy(() => import("./layouts/MainLayout"))
+const AuthLayout = React.lazy(() => import("./layouts/AuthLayout"))
+const Login = React.lazy(() => import("./pages/auth/Login"))
+const Register = React.lazy(() => import("./pages/auth/Register"))
+const Forgot = React.lazy(() => import("./pages/auth/Forgot"))
 
 // Halaman error
 const Error400 = () => <ErrorPage code="400" description="Bad Request" />;
@@ -13,13 +30,11 @@ const Error403 = () => <ErrorPage code="403" description="Forbidden" />;
 const NotFound = () => <ErrorPage code="404" description="Page Not Found" />;
 
 function App() {
+
   return (
-    <div id="app-container" className="bg-gray-100 min-h-screen flex">
-      <div id="layout-wrapper" className="flex flex-row flex-1">
-        <Sidebar />
-        <div id="main-content" className="flex-1 p-4">
-          <Header />
-          <Routes>
+    <Suspense fallback={<Loading />}>
+      <Routes>
+          <Route element={<MainLayout />}>
             <Route path="/" element={<Dashboard />} />
             <Route path="/orders" element={<Orders />} />
             <Route path="/customers" element={<Customers />} />
@@ -27,10 +42,15 @@ function App() {
             <Route path="/error-401" element={<Error401 />} />
             <Route path="/error-403" element={<Error403 />} />
             <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </div>
-    </div>
+          </Route>
+
+          <Route element={<AuthLayout/>}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register/>} />
+            <Route path="/forgot" element={<Forgot/>} />
+          </Route>
+      </Routes>
+      </Suspense>
   );
 }
 
